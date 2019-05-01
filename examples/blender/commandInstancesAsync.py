435484bf44db2505dd98ab14c56f002fe27c0103
Michael Coffey <mcoffey@yahoo.com>
2019-05-01 17:10:30 +0000

    commandInstancesAsync.py logs results to json-line file, reports summary; minor cleanup

--- examples/blender/commandInstancesAsync.py
+++ examples/blender/commandInstancesAsync.py
@@ -21,7 +21,7 @@ from eventTiming import eventTiming
 
 
 logger = logging.getLogger(__name__)
-
+resultsLogFile = None
 
 def anyFound( a, b ):
     ''' return true iff any items from iterable a is found in iterable b '''
@@ -30,6 +30,11 @@ def anyFound( a, b ):
             return True
     return False
 
+def logResult( key, value, instanceId ):
+    if resultsLogFile:
+        toLog = {key: value, 'instanceId': instanceId,
+            'dateTime': datetime.datetime.now(datetime.timezone.utc).isoformat() }
+        print( json.dumps( toLog, sort_keys=True ), file=resultsLogFile )
 
 def extractSshTargets( inRecs ):
     targets = []
@@ -51,24 +56,29 @@ async def run_client(inst, cmd):
     host = sshSpecs['host']
     port = sshSpecs['port']
     user = sshSpecs['user']
+    iid = inst['instanceId']
     # implement pasword-passing if present in ssh args
     password = sshSpecs.get('password', None )
 
     async with asyncssh.connect(host, port=port, username=user, password=password, known_hosts=None) as conn:
         async with conn.create_process(cmd) as proc:
             async for line in proc.stdout:
-                print('stdout[%s] %s' % (host, line), end='')
+                logger.info('stdout[%s] %s', host[0:16], line.strip() )
+                logResult( 'stdout', line.strip(), iid )
+
             async for line in proc.stderr:
-                print('stderr[%s] %s' % (host, line), end='')
+                logger.info('stderr[%s] %s', host[0:16], line.strip() )
+                #print('stderr[%s] %s' % (host, line), end='')
+                logResult( 'stderr', line.strip(), iid )
         await proc.wait_closed()
-        if proc.returncode is not None:
+        logResult( 'returncode', proc.returncode, iid )
+        if proc.returncode is None:
+            logger.warning( 'returncode[%s] NONE', host )
+            logger.info( 'CONN: %s', sorted(dir(conn)) )
+        else:
             print( 'returncode[%s] %d' % (host, proc.returncode) )
         return proc.returncode
-
-    '''
-    async with asyncssh.connect(host, port=port, username=user, password=password, known_hosts=None) as conn:
-        return await conn.run(command)
-    '''
+    return 'did we not connect?'
 
 async def run_client_orig(host, command):
     async with asyncssh.connect(host) as conn:
@@ -82,37 +92,35 @@ async def run_multiple_clients( instances, cmd, timeLimit=30 ):
                  for inst in instances )
     results = await asyncio.gather(*tasks, return_exceptions=True)
 
+    nGood = 0
+    nFailed = 0
+    nTimedOut = 0
+    nOther = 0
+    
     for i, result in enumerate(results, 1):
+        inst = instances[i-1]
+        iid = inst['instanceId']
+        abbrevIid = iid[0:16]
+
         if isinstance(result, asyncio.TimeoutError):
-            print('Task %d timed out.' % i)
+            nTimedOut += 1
+            logger.warning('task timed out for %s', abbrevIid )
         elif isinstance(result, int):
             # normally, each result is a return code from the remote
             #print('Task %d result: %s' % (i, result))
-            inst = instances[i-1]
-            iid = inst['instanceId']
-            logger.info( 'result code %d for %s', result, iid[0:16] )
+            if result:
+                nFailed += 1
+                logger.warning( 'result code %d for %s', result, abbrevIid )
+            else:
+                nGood += 1
+                #logger.debug( 'result code %d for %s', result, iid[0:16] )
         else:
             # unexpected result type
-            print('Task %d result: %s' % (i, result))
+            nOther += 1
+            logger.warning('task result for %s was (%s) %s', iid[0:16], type(result), result )
 
-    '''
-    tasks = (run_client(inst, cmd) for inst in instances)
-    #tasks = (run_client(host, cmd) for host in hosts)
-    results = await asyncio.gather(*tasks, return_exceptions=True)
-
-    for i, result in enumerate(results, 1):
-        if isinstance(result, Exception):
-            print('Task %d failed: %s' % (i, str(result)))
-        elif result.exit_status != 0:
-            print('Task %d exited with status %s:' % (i, result.exit_status))
-            print(result.stderr, end='')
-        else:
-            print('Task %d succeeded:' % i)
-            print(result.stdout, end='')
-
-        print(45*'-')
-    '''
-
+    logger.info( '%d good, %d failed, %d timed out, %d other',
+        nGood, nFailed, nTimedOut, nOther )
 
 if __name__ == "__main__":
     # configure logging
@@ -124,18 +132,25 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser( description=__doc__ )
     ap.add_argument('launchedJsonFilePath', default='launched.json')
     ap.add_argument('outJsonFilePath', default='installed.json')
-    ap.add_argument('--timeLimit', default=0, type=float, help='maximum time (in seconds) to take (default =unlimited (0))')
+    ap.add_argument('--timeLimit', type=float, help='maximum time (in seconds) to take (default=none (unlimited)')
     ap.add_argument('--command', default='uname', help='the command to execute')
     args = ap.parse_args()
     logger.info( "args: %s", str(args) )
     
+    dataDirPath = 'data'
     launchedJsonFilePath = args.launchedJsonFilePath
 
     timeLimit = args.timeLimit  # seconds
 
+
+    startDateTime = datetime.datetime.now( datetime.timezone.utc )
     startTime = time.time()
-    deadline = startTime + timeLimit
-    
+    #deadline = startTime + timeLimit
+
+    dateTimeTagFormat = '%Y-%m-%d_%H%M%S'  # cant use iso format dates in filenames because colons
+    dateTimeTag = startDateTime.strftime( dateTimeTagFormat )
+    logger.info( 'dateTimeTag is %s', dateTimeTag )
+
     loadedInstances = None
     with open( launchedJsonFilePath, 'r' ) as jsonFile:
         loadedInstances = json.load(jsonFile)  # a list of dicts
@@ -160,10 +175,17 @@ if __name__ == "__main__":
     eventTimings.append(starterTiming)
 
        
-    resultsLogFilePath = os.path.splitext( os.path.basename( __file__ ) )[0] + '_results.log'
-    resultsLogFile = open( resultsLogFilePath, "a", encoding="utf8" )
-    print( '[]', datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
-        "args:", str(args), file=resultsLogFile )
+    #resultsLogFilePath = os.path.splitext( os.path.basename( __file__ ) )[0] + '_results.log'
+    resultsLogFilePath = dataDirPath + '/' \
+        + os.path.splitext( os.path.basename( __file__ ) )[0] \
+        + '_results_' + dateTimeTag  + '.log'
+
+    resultsLogFile = open( resultsLogFilePath, "w", encoding="utf8" )
+    #print( '[]', datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
+    #    "args:", str(args), file=resultsLogFile )
+    #toLog = {'startDateTime': datetime.datetime.now().isoformat(), 'args': vars(args) }
+    #print( json.dumps( toLog ), file=resultsLogFile )
+    logResult( 'programArgs', vars(args), '<master>')
     
     installed = set()
     failed = set()
