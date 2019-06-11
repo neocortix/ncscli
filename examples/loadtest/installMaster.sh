594c59b23a3516413dfda5ebec6c3f74f0195a0c
Michael Coffey <mcoffey@yahoo.com>
2019-06-11 22:14:37 +0000

    installMaster.sh has "-y" on its apt-get commands

--- examples/loadtest/installMaster.sh
+++ examples/loadtest/installMaster.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install ansible
-sudo apt-get install python3-pip rsync
+sudo apt-get install -y ansible
+sudo apt-get install -y python3-pip rsync
 pip3 install --user flask gevent msgpack-python pyzmq python-dateutil geoip2 pandas
 pushd ~
 [ ! -d "ncscli" ] && git clone https://github.com/neocortix/ncscli.git
