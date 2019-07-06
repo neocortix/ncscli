c739e90d2f73083e8593449e159058243377f9f5
Michael Coffey <mcoffey@yahoo.com>
2019-07-06 17:52:54 +0000

    locustfile.py index() uses "" instead of "/"

--- examples/loadtest/locustfile.py
+++ examples/loadtest/locustfile.py
@@ -8,7 +8,7 @@ def login(l):
     l.client.post("/login", {"username":"ellen_key", "password":"education"})
 
 def index(l):
-    l.client.get("/")
+    l.client.get("")
 
 def index_orig(l):
     l.client.get("/index_orig.html")
