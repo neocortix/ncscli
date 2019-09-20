3b4bb0aefadbd4f3143ef1ea744059d81f312640
Michael Coffey <mcoffey@yahoo.com>
2019-09-20 18:22:29 +0000

    jsonToKnownHosts.py has importable function, code consistent with runDistributedBlender

--- examples/blender/jsonToKnownHosts.py
+++ examples/blender/jsonToKnownHosts.py
@@ -11,29 +11,17 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    logging.basicConfig()
-    logger.setLevel(logging.INFO)
-    logger.debug( 'the logger is configured' )
 
-    ap = argparse.ArgumentParser( description=__doc__, fromfile_prefix_chars='@' )
-    args = ap.parse_args()
-    #logger.info( 'args %s', args )
-
-    inRecs = json.load(sys.stdin)
-
+def jsonToKnownHosts( instances, outFile ):
     outLines = []
-    for inRec in inRecs:
+    for inRec in instances:
         details = inRec
-        iid = details['instanceId']
-        #logger.info( 'NCSC Inst details %s', details )
         if 'commandState' in details and details['commandState'] != 'good':
             continue
         if details['state'] == 'started':
             if 'ssh' in details:
                 host = details['ssh']['host']
                 port = details['ssh']['port']
-                user = details['ssh']['user']
                 ecdsaKey = details['ssh']['host-keys']['ecdsa']
                 try:
                     ipAddr = socket.gethostbyname( host )
@@ -45,4 +33,16 @@ if __name__ == "__main__":
                         )
                     outLines.append( outLine )
     for outLine in sorted( outLines):
-        print( outLine )
+        print( outLine, file=outFile )
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logger.setLevel(logging.INFO)
+    logger.debug( 'the logger is configured' )
+
+    ap = argparse.ArgumentParser( description=__doc__, fromfile_prefix_chars='@' )
+    args = ap.parse_args()
+    #logger.info( 'args %s', args )
+
+    inRecs = json.load(sys.stdin)
+    jsonToKnownHosts( inRecs, sys.stdout )
