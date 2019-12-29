cd504af0356b10deccbfd7c663cfdbebf70078a0
Michael Coffey <mcoffey@yahoo.com>
2019-12-29 18:13:39 +0000

    postLogs.py has improved indexing, --official to post a summary of the given test

--- examples/blender/postLogs.py
+++ examples/blender/postLogs.py
@@ -30,14 +30,15 @@ def postCollection( fileName, collName=None ):
     ]
     if fileName.endswith( '.json' ):
         cmd.append( '--jsonArray' )
-    logger.info( 'cmd: %s', cmd )
+    #logger.info( 'cmd: %s', cmd )
     subprocess.check_call( cmd )
 
     collection = logsDb[collectionName]
-    print( '%s has %d documents' % (collectionName, collection.count_documents({})) )
-    collection.create_index( 'dateTime' )
-    collection.create_index( 'instanceId' )
-    collection.create_index( 'type' )
+    logger.info( '%s has %d documents', collectionName, collection.count_documents({}) )
+    #collection.create_index( 'dateTime' )
+    #collection.create_index( 'instanceId' )
+    #collection.create_index( 'type' )
+    return collection
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
@@ -49,6 +50,7 @@ if __name__ == "__main__":
     ap.add_argument( '--tag', required=True, help='tag for data dir and collection names' )
     ap.add_argument( '--server', default='localhost', help='the host of the mongodb server' )
     ap.add_argument( '--port', default=27017, help='the port for the mongodb server' )
+    ap.add_argument( '--official', action='store_true', help='to add this to the table of official tests' )
     args = ap.parse_args()
 
     mclient = pymongo.MongoClient(args.server, args.port)
@@ -57,6 +59,40 @@ if __name__ == "__main__":
     logger.info( 'existing collections %s', collections )
 
 
-    postCollection( 'recruitLaunched.json', 'launchedInstances' )
-    postCollection( 'recruitInstances.jlog', 'installerLog' )
-    postCollection( 'animateWholeFrames_results.jlog', 'rendererLog' )
+    record = {'tag': args.tag, '_id': args.tag }
+
+    coll = postCollection( 'recruitLaunched.json', 'launchedInstances' )
+    record['launchedInstances'] = coll.name
+    coll.create_index( 'instanceId' )
+
+    coll = postCollection( 'recruitInstances.jlog', 'installerLog' )
+    record['installerLog'] = coll.name
+    coll.create_index( 'instanceId' )
+    coll.create_index( 'dateTime' )
+   
+    coll = postCollection( 'animateWholeFrames_results.jlog', 'rendererLog' )
+    record['rendererLog'] = coll.name
+    coll.create_index( 'instanceId' )
+    coll.create_index( 'dateTime' )
+    coll.create_index( 'type' )
+
+    if args.official:
+        # get info about the test from the renderer log collection
+        query = {'type': 'operation'}
+        firstOp = coll.find_one( query, hint=[('dateTime', pymongo.ASCENDING)] )
+        record['dateTime'] = firstOp['dateTime']
+        opArgs = firstOp['args']
+        startingArgs = opArgs.get( 'starting' )
+        if startingArgs:
+            record['blendFilePath'] = startingArgs.get('blendFilePath')
+            startFrame = startingArgs.get('startFrame', 0)
+            endFrame = startingArgs.get('endFrame')
+            frameStep = startingArgs.get('frameStep')
+            if endFrame and frameStep:
+                nFrames = len( range( startFrame, endFrame+1, frameStep) )
+                record['nFramesReq'] = nFrames
+        logger.info( 'storing officially as %s', record['_id'] )
+        officialColl = logsDb['officialTests']
+        result = officialColl.replace_one( {'_id': record['_id']}, record, upsert=True )
+        #officialColl.insert_one( record )
+        #logger.info( 'result %s', result )
