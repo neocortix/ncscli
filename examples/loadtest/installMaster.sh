0af171a2fc36fd1d244a54a328a91d97be73f036
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-08-27 19:17:03 +0000

    loadtest/installMaster.sh installs specific versions of locust, gevent, and matplotlib

--- examples/loadtest/installMaster.sh
+++ examples/loadtest/installMaster.sh
@@ -3,8 +3,8 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 sudo apt-get install -y python3-pip rsync
-pip3 install --user flask gevent msgpack-python pyzmq python-dateutil geoip2 pandas
+pip3 install --user flask gevent==1.4.0 msgpack-python pyzmq python-dateutil geoip2 pandas matplotlib==3.1.1
 pushd ~
 [ ! -d "ncscli" ] && git clone https://github.com/neocortix/ncscli.git
-[ ! -d "locust" ] && git clone https://github.com/locustio/locust.git
+[ ! -d "locust" ] && git clone --branch 0.12.2 https://github.com/locustio/locust.git
 popd
