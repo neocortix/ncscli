fb99961ec6b6d1c5c041076091bd9b78757575c0
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-09-27 00:18:45 +0000

    new pytestQuick.sh runs pytest, deselecting the longer examples

---
+++ examples/batchMode/pytestQuick.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pytest -v --junitxml data/pytest.xml -o 'junit_family = xunit2' \
+  --deselect test_runBatchExamples.py::test_runBatchBlender \
+  --deselect test_runBatchExamples.py::test_runBatchJMeter \
+  --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse
+
+#   --deselect  test_runBatchExamples.py::test_runBatchPython
