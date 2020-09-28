840f18a878247d7543bb94b63770023a61bc3e5e
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-09-28 21:01:12 +0000

    pytestQuick.sh sets --rootdir =.

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pytest -v --junitxml data/pytest.xml -o 'junit_family = xunit2' \
+pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect test_runBatchExamples.py::test_runBatchBlender \
   --deselect test_runBatchExamples.py::test_runBatchJMeter \
   --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse
