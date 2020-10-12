74267b6eb557e891ebc3e16b3b24d896f51a01c0
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-10-12 23:27:06 +0000

    pytestQuick.sh calls xmllint for pretty-printing, if available

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -6,3 +6,7 @@ pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse
 
 #   --deselect  test_runBatchExamples.py::test_runBatchPython
+
+# pretty-print the xml summary, if xmllint is installed
+if hash xmllint 2>/dev/null; then
+    xmllint --format data/pytest.xml
