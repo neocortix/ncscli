746c66a074ace181c618648c96aa73b7807d0553
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-10-13 01:44:47 +0000

    runBatchK6.py uses compressed k6 binary, avoids overriding script parameters

--- examples/batchMode/runBatchK6.py
+++ examples/batchMode/runBatchK6.py
@@ -11,15 +11,16 @@ import ncscli.batchRunner as batchRunner
 class k6FrameProcessor(batchRunner.frameProcessor):
     '''defines details for using k6 for a simplistic load test'''
 
+    def installerCmd( self ):
+        return 'tar -vzxf k6Worker/k6.tar.gz; ls -al k6Worker'
+
     def frameOutFileName( self, frameNum ):
         return 'worker_%03d.*' % frameNum
         #return 'worker_%03d.csv' % frameNum
 
     def frameCmd( self, frameNum ):
-        usersPerWorker = 6  # number of simulated users per worker instance
-        duration = 90  # number of seconds to run the test
-        cmd = 'cd k6Worker && ./k6 run -q script.js --vus %d --duration %.1fs --out csv=~/worker_%03d.csv --out json=~/worker_%03d.json' % (
-            usersPerWorker, duration, frameNum, frameNum
+        cmd = 'cd k6Worker && ./k6 run -q script.js --out csv=~/worker_%03d.csv' % (
+            frameNum
         )
         return cmd
 
@@ -36,8 +37,8 @@ logging.basicConfig(format=logFmt, datefmt=logDateFmt)
 dateTimeTag = datetime.datetime.now().strftime( '%Y-%m-%d_%H%M%S' )
 outDataDir = 'data/k6_' + dateTimeTag
 
-if not os.path.isfile( 'k6Worker/k6' ):
-    logger.error( 'the k6 Arm-64 binary was not found, you may need to build it using "go get"' )
+if not os.path.isfile( 'k6Worker/k6.tar.gz' ):
+    logger.error( 'the compressed k6 binary was not found, you may need to build and compress it' )
     sys.exit( 1)
 try:
     rc = batchRunner.runBatch(
@@ -46,8 +47,8 @@ try:
         authToken = os.getenv('NCS_AUTH_TOKEN') or 'YourAuthTokenHere',
         encryptFiles=False,
         timeLimit = 80*60,
-        instTimeLimit = 8*60,
-        frameTimeLimit = 10*60,
+        instTimeLimit = 7*60,
+        frameTimeLimit = 14*60,
         filter = '{"dpr": ">=48","ram:":">=2800000000","app-version": ">=2.1.11"}',
         outDataDir = outDataDir,
         startFrame = 1,
