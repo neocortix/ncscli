399dfdab6652ff4e45e86494009f68644a9784d2
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-10-22 22:59:11 +0000

    pytestQuick.sh no longer deselects test_runBatchJMeter

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -2,10 +2,10 @@
 
 pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect test_runBatchExamples.py::test_runBatchBlender \
-  --deselect test_runBatchExamples.py::test_runBatchJMeter \
   --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse
 
 #   --deselect  test_runBatchExamples.py::test_runBatchPython
+#   --deselect test_runBatchExamples.py::test_runBatchJMeter \
 
 # pretty-print the xml summary, if xmllint is installed
 if hash xmllint 2>/dev/null; then
