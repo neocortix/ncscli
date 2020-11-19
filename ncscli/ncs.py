a3893570a6a467967aea37500e2894b7e59bbb7b
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-11-19 19:40:48 +0000

    set version to 0.12.14 in setup.py and ncs.py

--- ncscli/ncs.py
+++ ncscli/ncs.py
@@ -18,7 +18,7 @@ import uuid
 # third-party modules
 import requests
 
-__version__ = '0.12.13'
+__version__ = '0.12.14'
 logger = logging.getLogger(__name__)
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 import shutil
 
-__version__ = '0.12.13'
+__version__ = '0.12.14'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
