172a7b85f379cfe5003e3d4d90f2ba7dd1e5e158
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2020-12-09 21:04:10 +0000

    nlAgent/install.sh uses curl, avoids installing wget and fontconfig

--- examples/neoload/nlAgent/install.sh
+++ examples/neoload/nlAgent/install.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get -qq update
 
-sudo apt-get install -y wget
-sudo apt-get install -y fontconfig  # not sure if this is needed
-
-wget --no-verbose https://d24mnm5myvorwj.cloudfront.net/documents/download/neoload/v7.6/loadGenerator_7_6_0_linux_x64.tar.gz
+curl -L -s -S https://d24mnm5myvorwj.cloudfront.net/documents/download/neoload/v7.6/loadGenerator_7_6_0_linux_x64.tar.gz > loadGenerator_7_6_0_linux_x64.tar.gz
 tar zxf loadGenerator_7_6_0_linux_x64.tar.gz
 
 cp nlAgent/agent.properties ~/neoload7.6/conf
-ls -al ~/neoload7.6/conf
-
