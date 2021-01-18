b10b33913a63453b2e3590e43874e3dfe16eb6ef
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-01-18 22:21:54 +0000

    pytestQuick.sh deselects test_runBatchTaurus

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -3,6 +3,7 @@
 pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect test_runBatchExamples.py::test_runBatchBlender \
   --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse \
+  --deselect  test_runBatchExamples.py::test_runBatchTaurus \
   --ignore DevWeb
 
 #   --deselect  test_runBatchExamples.py::test_runBatchPython
