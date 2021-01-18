a62fbe5c669c493a43f2303d862e6e6744cccd09
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-01-18 20:39:30 +0000

    pytestQuick.sh has --ignore DevWeb

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -2,7 +2,8 @@
 
 pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect test_runBatchExamples.py::test_runBatchBlender \
-  --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse
+  --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse \
+  --ignore DevWeb
 
 #   --deselect  test_runBatchExamples.py::test_runBatchPython
 #   --deselect test_runBatchExamples.py::test_runBatchJMeter \
