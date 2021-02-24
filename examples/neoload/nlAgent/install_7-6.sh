09e5b83b92d152f773a6412049799774c794bee3
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-02-24 21:09:54 +0000

    nlAgent/install_7-6.sh copies all config files

--- examples/neoload/nlAgent/install_7-6.sh
+++ examples/neoload/nlAgent/install_7-6.sh
@@ -3,4 +3,4 @@
 curl -L -s -S https://d24mnm5myvorwj.cloudfront.net/documents/download/neoload/v7.6/loadGenerator_7_6_0_linux_x64.tar.gz > loadGenerator_7_6_0_linux_x64.tar.gz
 tar zxf loadGenerator_7_6_0_linux_x64.tar.gz
 
-cp nlAgent/agent.properties ~/neoload7.6/conf
+cp nlAgent/*.properties ~/neoload7.6/conf
