30df75ac11b46166750c3815f2426da6b007b12c
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-04-03 18:08:49 +0000

    checkGethNodes.py has maxNewAuths, checkInstanceClocks() and waitForAuth(), uses historicSigners and ncsgeth.collectSignerInstances

--- examples/ethereum/checkGethNodes.py
+++ examples/ethereum/checkGethNodes.py
@@ -11,6 +11,7 @@ import logging
 import os
 import subprocess
 import sys
+import time
 # third-party modules
 import dateutil.parser
 import psutil
@@ -35,6 +36,38 @@ def universalizeDateTime( dt ):
         #return dt
         return dt.astimezone(datetime.timezone.utc)
     return dt.replace( tzinfo=datetime.timezone.utc )
+
+def checkInstanceClocks( liveInstances, dataDirPath ):
+    jlogFilePath = dataDirPath + '/checkInstanceClocks.jlog'
+    cmd = "date --iso-8601=seconds"
+    # check for a running geth process on each instance
+    stepStatuses = tellInstances.tellInstances( liveInstances, cmd,
+        timeLimit=2*60,
+        resultsLogFilePath = jlogFilePath,
+        knownHostsOnly=True, sshAgent=not True
+        )
+    #logger.info( 'proc statuses: %s', stepStatuses )
+    errorsByIid = {status['instanceId']: status['status'] for status in stepStatuses if status['status'] }
+    logger.info( 'preliminary errorsByIid: %s', errorsByIid )
+    for iid, status in errorsByIid.items():
+        logger.warning( 'instance %s gave error "%s"', iid, status )
+
+    with open( jlogFilePath, 'rb' ) as inFile:
+        for line in inFile:
+            decoded = json.loads( line )
+            if 'stdout' in decoded:
+                #logger.info( decoded )
+                masterDateTime = dateutil.parser.parse( decoded['dateTime'] )
+                nodeDateTime = dateutil.parser.parse( decoded['stdout'] )
+                delta = masterDateTime - nodeDateTime
+                discrep =delta.total_seconds()
+                logger.info( 'discrep: %.1f seconds on inst %s',
+                    discrep, decoded['instanceId'] )
+                if discrep > 4 or discrep < -1:
+                    logger.warning( 'bad time discrep: %.1f', discrep )
+                    errorsByIid[ decoded['instanceId'] ] = {'discrep': discrep }
+    logger.info( '%d errorsByIid: %s', len(errorsByIid), errorsByIid )
+    return errorsByIid
 
 def checkProcesses( liveInstances ):
     logger.info( 'checking %d instance(s)', len(liveInstances) )
@@ -102,27 +135,37 @@ def checkLogs( liveInstances, dataDirPath ):
                 )
             '''
             connected = False
+            sealed = False
             with open( logFilePath ) as logFile:
                 for line in logFile:
                     line = line.rstrip()
                     if not line:
                         continue
                     if 'ERROR' in line:
+                        if 'Ethereum peer removal failed' in line:
+                            logger.info( 'ignoring "peer removal failed"')
+                            continue
                         logger.info( '%s: %s', iidAbbrev, line )
                         theseErrors = errorsByIid.get( iid, [] )
                         theseErrors.append( line )
                         errorsByIid[iid] = theseErrors
-                    if 'Started P2P networking' in line:
+                    elif 'Started P2P networking' in line:
                         connected = True
+                    elif 'Successfully sealed' in line:
+                        sealed = True
                 if not connected:
                     logger.warning( 'instance %s did not initialize fully', iidAbbrev )
+                if sealed:
+                    logger.info( 'instance %s has successfully sealed', iidAbbrev )
     logger.info( 'found errors for %d instance(s)', len(errorsByIid) )
     #print( 'errorsByIid', errorsByIid  )
     summaryCsvFilePath = os.path.join( dataDirPath, 'errorSummary.csv' )
+    fileExisted = os.path.isfile( summaryCsvFilePath )
     with open( summaryCsvFilePath, 'a' ) as csvOutFile:
-        print( 'instanceId', 'msg',
-            sep=',', file=csvOutFile
-            )
+        if not fileExisted:
+            print( 'instanceId', 'msg',
+                sep=',', file=csvOutFile
+                )
         for iid in errorsByIid:
             lines = errorsByIid[iid]
             logger.info( 'saving %d errors for %s', len(lines), iid[0:8])
@@ -145,22 +188,20 @@ def findAuthorizers( instances, savedSigners, badIids ):
                 logger.info( 'including authorizer %s', iid )
     return authorizers
 
-
-def parseLogLevel( arg ):
-    '''return a logging level (int) for the given case-insensitive level name'''
-    arg = arg.lower()
-    map = { 
-        'critical': logging.CRITICAL,
-        'error': logging.ERROR,
-        'warning': logging.WARNING,
-        'info': logging.INFO,
-        'debug': logging.DEBUG
-        }
-    if arg not in map:
-        logger.warning( 'the given logLevel "%s" is not recognized (using "info" level, instead)', arg )
-    setting = map.get( arg, logging.INFO )
-
-    return setting
+def waitForAuth( victimAccount, shouldAuth, instances, configName, timeLimit ):
+    '''wait for the authorization of victimAccount to match shouldAuth; return success (true or false)'''
+    curSigners = ncsgeth.collectAuthSigners( instances, configName )
+    nowAuth = victimAccount in curSigners
+    logger.info( 'now authorized? %s', nowAuth )
+    deadline = time.time() + timeLimit
+    while nowAuth != shouldAuth:
+        if time.time() >= deadline:
+            logger.warning( 'took too long for auth/deauth to propagate')
+            break
+        curSigners = ncsgeth.collectAuthSigners( instances, configName )
+        nowAuth = victimAccount in curSigners
+        logger.info( 'now authorized? %s', nowAuth )
+    return nowAuth == shouldAuth
 
 
 if __name__ == "__main__":
@@ -179,7 +220,7 @@ if __name__ == "__main__":
     ap.add_argument( '--logLevel', default ='info', help='verbosity of log (e.g. debug, info, warning, error)' )
     args = ap.parse_args()
 
-    logLevel = parseLogLevel( args.logLevel )
+    logLevel = ncsgeth.parseLogLevel( args.logLevel )
     logger.setLevel(logLevel)
     tellInstances.logger.setLevel( logLevel )
     logger.debug('the logger is configured')
@@ -219,14 +260,18 @@ if __name__ == "__main__":
     startedIids = [inst['instanceId'] for inst in startedInstances ]
     #startedIids = [inst['instanceId'] for inst in launchedInstances if inst['state'] == 'started' ]
 
+
     savedSignersFilePath = os.path.join( dataDirPath, 'savedSigners.json' )
-    savedSigners = {}
+    historicSigners = {}
     if os.path.isfile( savedSignersFilePath ):
         with open( savedSignersFilePath, 'r') as jsonInFile:
             try:
-                savedSigners = json.load(jsonInFile) # a dict of lists, indexed by iid
+                historicSigners = json.load(jsonInFile) # a dict of lists, indexed by iid
             except Exception as exc:
                 logger.warning( 'could not load savedSigners json (%s) %s', type(exc), exc )
+    if not historicSigners:
+        logger.info( 'there are no saved signers' )
+
 
     # query cloudserver to see which instances are still live
     logger.info( 'querying to see which instances are still live' )
@@ -264,6 +309,9 @@ if __name__ == "__main__":
     errorsByIid = checkProcesses( liveInstances )
     badIids.extend( list( errorsByIid.keys() ) )
 
+    errorsByIid = checkInstanceClocks( liveInstances, dataDirPath )
+    badIids.extend( list( errorsByIid.keys() ) )
+
 
     errorsByIid = retrieveLogs( liveInstances, farmDirPath )
     badIids.extend( list( errorsByIid.keys() ) )
@@ -271,6 +319,29 @@ if __name__ == "__main__":
     errorsByIid = checkLogs( liveInstances, dataDirPath )
     if errorsByIid:
         badIids.extend( list( errorsByIid.keys() ) )
+    goodInstances = [inst for inst in liveInstances if inst['instanceId'] not in badIids ]
+
+
+
+    signerInfos = ncsgeth.collectSignerInstances( anchorInstances+goodInstances, args.configName )  # a list
+    logger.info( '%d signerInfos: %s', len( signerInfos), signerInfos )
+    signersByIid = {signer['instanceId']: signer for signer in signerInfos }
+    # conversion code to produce legacy savedSigners
+    savedSigners = {}
+    for signerInfo in signerInfos:
+        savedSigners[ signerInfo['instanceId'] ] = [signerInfo['accountAddr']]
+    logger.info( '%d savedSigners: %s', len( savedSigners), savedSigners )
+    if not savedSigners:
+        logger.info( 'there are NO signers' )
+
+
+    # can start all miners, if any
+    if False:
+        if savedSigners:
+            authorizers = findAuthorizers( anchorInstances+goodInstances, savedSigners, badIids )
+            if authorizers:
+                logger.info( 'STARTING ALL %d MINERS', len( authorizers ) )
+                ncsgeth.startMiners( authorizers, configName )
 
     authorizers = []
     terminatedIids = []
@@ -278,50 +349,67 @@ if __name__ == "__main__":
         # deduplicate badIids but leave it as a list
         badIids = list( set( badIids ) )
         # build list of instances capable of voting on authorization
-        if not authorizers:
-            authorizers = findAuthorizers( anchorInstances+liveInstances, savedSigners, badIids )
+        authorizers = findAuthorizers( anchorInstances+liveInstances, savedSigners, badIids )
         logger.info( '%d authorizers', len(authorizers) )
         # terminate bad instances, deauthorizing any that are also authorized
         for iid in badIids:
-            #logger.info( 'thinking about deauthorizing %s', iid[0:16])
-            wasSigner = iid in savedSigners
+            wasSigner = (iid in savedSigners) or (iid in historicSigners)
             #logger.info( 'saved signer? %s', wasSigner )
             if wasSigner:
+                logger.info( 'will deauthorize %s', iid[0:16])
                 # victim is first account in savedSigners list for this instance
-                victimAccount = savedSigners[iid][0]
+                logger.info( 'sleeping for 90 seconds' )
+                time.sleep( 90 )
+                victimAccount = historicSigners[iid][0]
                 logger.info( 'deauthorizing %s account %s', iid[0:16], victimAccount )
                 results = ncsgeth.authorizeSigner( authorizers, configName, victimAccount, False )
                 logger.info( 'authorizeSigner returned: %s', results )
+                waitForAuth( victimAccount, False, authorizers, configName, timeLimit=15*60 )
             logger.info( 'terminating %s', iid)
             ncs.terminateInstances( authToken, badIids )
             terminatedIids.append( iid )
 
     # authorize good instances that have been up for long enough
+    trustedThresh = 12  # hours
+    maxNewAuths = 0
+    nNewAuths = 0
+    sleepAmt = 15
+    if badIids:
+        sleepAmt = 90  # longer to avoid possible trouble
     now = datetime.datetime.now( datetime.timezone.utc )
-    goodInstances = [inst for inst in liveInstances if inst['instanceId'] not in badIids ]
     for inst in goodInstances:
+        # limit the loop to <= maxNewAuths auths per run
         iid = inst['instanceId']
         abbrevIid = iid[0:16]
         startedAtStr = inst['started-at']
         #logger.info( 'good instance %s started at %s', abbrevIid, startedAtStr )
         startedDateTime = universalizeDateTime( dateutil.parser.parse( startedAtStr ) )
         uptimeHrs = (now - startedDateTime).total_seconds() / 3600
-        #logger.info( 'uptime %.1f hrs', uptimeHrs )
-        trustedThresh = 480
+        logger.info( 'uptime %.1f hrs', uptimeHrs )
+        if nNewAuths >= maxNewAuths:
+            break
         if uptimeHrs >= trustedThresh:
             wasSigner = iid in savedSigners
             if not wasSigner:
                 results = ncsgeth.collectPrimaryAccounts( [inst], configName )
                 if results[0]:
-                    if not authorizers:
-                        authorizers = findAuthorizers( anchorInstances+liveInstances, savedSigners, badIids )
+                    authorizers = findAuthorizers( anchorInstances+liveInstances, savedSigners, badIids )
                     logger.info( '%d authorizers', len(authorizers) )
+                    if not authorizers:
+                        break
                     iidAccPair = results[0]
                     primaryAccount = iidAccPair['accountAddr']
                     #logger.info( 'primary account: %s', primaryAccount )
+                    logger.info( 'sleeping for %.1f seconds', sleepAmt )
+                    time.sleep( sleepAmt )
+                    sleepAmt = 90  # longer sleep after the first auth
+                    logger.info( 'starting miner on %s', iid[0:16] )
+                    minerResults = ncsgeth.startMiners( [inst], configName )
                     logger.info( 'authorizing %s account %s', iid[0:16], primaryAccount )
                     authResults = ncsgeth.authorizeSigner( authorizers, configName, primaryAccount, True )
                     logger.info( 'authResults:  %s', authResults )
+                    waitForAuth( primaryAccount, True, authorizers, configName, timeLimit=15*60 )
+                    nNewAuths += 1
 
     terminatedIids = set( terminatedIids )
     stillLive = [inst for inst in liveInstances if inst['instanceId'] not in terminatedIids]
