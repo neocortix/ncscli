f893163d3f46f22b489ef2b45a5e38dd23fab65f
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-05-22 19:34:15 +0000

    batchMode/pytestQuick.sh ignores the go directory

--- examples/batchMode/pytestQuick.sh
+++ examples/batchMode/pytestQuick.sh
@@ -4,7 +4,8 @@ pytest -v --rootdir . --junitxml data/pytest.xml -o 'junit_family = xunit2' \
   --deselect test_runBatchExamples.py::test_runBatchBlender \
   --deselect  test_runBatchExamples.py::test_runBatchPuppeteerLighthouse \
   --deselect  test_runBatchExamples.py::test_runBatchTaurus \
-  --ignore DevWeb
+  --ignore DevWeb \
+  --ignore go
 
 #   --deselect  test_runBatchExamples.py::test_runBatchPython
 #   --deselect test_runBatchExamples.py::test_runBatchJMeter \
