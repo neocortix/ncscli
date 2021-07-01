d3700284354e4d88008dd3323b286d7567103136
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-07-01 18:43:01 +0000

    runBatchJMeter.py has memory improvements and can use perfmonClient.py, uses only usa devices and dar==100

--- examples/batchMode/runBatchJMeter.py
+++ examples/batchMode/runBatchJMeter.py
@@ -12,7 +12,9 @@ class JMeterFrameProcessor(batchRunner.frameProcessor):
     '''defines details for using JMeter for a simplistic load test'''
 
     def installerCmd( self ):
-        return '/opt/apache-jmeter/bin/jmeter.sh --version'
+        cmd = 'free --mega -t 1>&2'
+        cmd += ' && JVM_ARGS="%s" /opt/apache-jmeter/bin/jmeter.sh --version' % self.JVM_ARGS
+        return cmd
         # could install an alternative version of jmeter, if the preinstalled version is not wanted
         '''
         jmeterVersion = '5.4.1'  # 5.3 and 5.4.1 have been tested, others may work as well
@@ -25,13 +27,15 @@ class JMeterFrameProcessor(batchRunner.frameProcessor):
     #JMeterFilePath = 'TestPlan_RampLong.jmx'
     #JMeterFilePath = 'TestPlan_RampLong_MoreSlow.jmx'
     #JMeterFilePath = 'TestPlan_RampLong_LessSlow.jmx'
+    JVM_ARGS ='-Xms20m -Xmx212m -XX:MaxMetaspaceSize=64m -Dnashorn.args=--no-deprecation-warning'
+
 
     def frameOutFileName( self, frameNum ):
         return 'TestPlan_results_%03d.csv' % frameNum
 
     def frameCmd( self, frameNum ):
-        cmd = '/opt/apache-jmeter/bin/jmeter.sh -n -t %s -l TestPlan_results_%03d.csv -D httpclient4.time_to_live=1 -D httpclient.reset_state_on_thread_group_iteration=true' % (
-            self.JMeterFilePath, frameNum
+        cmd = 'JVM_ARGS="%s" /opt/apache-jmeter/bin/jmeter.sh -n -t %s -l TestPlan_results_%03d.csv -D httpclient4.time_to_live=1 -D httpclient.reset_state_on_thread_group_iteration=true' % (
+            self.JVM_ARGS, self.JMeterFilePath, frameNum
         )
         return cmd
 
@@ -48,7 +52,18 @@ logging.basicConfig(format=logFmt, datefmt=logDateFmt)
 dateTimeTag = datetime.datetime.now().strftime( '%Y-%m-%d_%H%M%S' )
 outDataDir = 'data/jmeter_' + dateTimeTag
 
+perfMonHost = None
+#perfMonHost = 'loadtest-target.neocortix.com'
+
+
 try:
+    monProc = None
+    if perfMonHost:
+        perfmonOutFilePath = os.path.join( outDataDir, 'perfmonOut.csv' )
+        monProc = subprocess.Popen( [sys.executable, 'perfmonClient.py',
+            '--outFilePath', perfmonOutFilePath, '--pmHost', perfMonHost,
+            '--pmPort', str(4444)
+            ])
     rc = batchRunner.runBatch(
         frameProcessor = JMeterFrameProcessor(),
         commonInFilePath = JMeterFrameProcessor.JMeterFilePath,
@@ -57,7 +72,8 @@ try:
         timeLimit = 80*60,
         instTimeLimit = 6*60,
         frameTimeLimit = 13*60,
-        filter = '{"dpr": ">=48","ram:":">=2800000000","app-version": ">=2.1.11"}',
+        filter = '{"regions": ["usa"], "dpr": ">=48", "dar": "==100", "ram":">=3800000000"}',
+        #filter = '{"regions": ["usa"], "dpr": ">=48", "dar": ">=90", "ram":">=2800000000"}',
         outDataDir = outDataDir,
         startFrame = 1,
         endFrame = 6,
@@ -65,6 +81,8 @@ try:
         limitOneFramePerWorker = True,
         autoscaleMax = 2
     )
+    if monProc:
+        monProc.terminate()
     if (rc == 0) and os.path.isfile( outDataDir +'/recruitLaunched.json' ):
         rampStepDuration = 60
         SLODuration = 240
