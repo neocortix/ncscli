d4cbefe8b2b0573cc25f900c588264eec69e91ba
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-07-27 18:08:22 +0000

    new nlAgent/install_7-10_slim.sh installs slim LG package from archive server

---
+++ examples/neoload/nlAgent/install_7-10_slim.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+curl -L -s -S https://archive-1.neocortix.net/neotys/neoload/v7.10/loadGenerator_7_10_0_slim.tar.gz > loadGenerator_7_10_0_slim.tar.gz
+#curl -L -s -S https://archive.neocortix.net/neotys/neoload/v7.10/loadGenerator_7_10_0_slim.tar.gz > loadGenerator_7_10_0_slim.tar.gz
+#curl -L -s -S https://d1ki6ltcdxd6oo.cloudfront.net/neotys/neoload/v7.10/loadGenerator_7_10_0_slim.tar.gz > loadGenerator_7_10_0_slim.tar.gz
+#curl -L -s -S https://d24mnm5myvorwj.cloudfront.net/documents/download/neoload/v7.10/loadGenerator_7_10_0_unix.tar.gz > loadGenerator_7_10_0_unix.tar.gz
+#mv nlAgent/loadGenerator_7_10_0_slim.tar.gz .
+tar zxf loadGenerator_7_10_0_slim.tar.gz
+
+cp nlAgent/*.properties ~/neoload7.10/conf
+# a symlink so neoload agent can find java to run the LG
+ln -s /usr/lib/jvm/java-11-openjdk-arm64 $HOME/neoload7.10/jre
+
+#COULD check available memory and bound ports
