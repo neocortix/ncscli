8274f5866b5bc98c35ecadcf24e6a3908e82f7c8
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-10-18 23:15:47 +0000

    set version to 1.1.30 in setup.py and ncs.py

--- ncscli/ncs.py
+++ ncscli/ncs.py
@@ -19,7 +19,7 @@ import uuid
 # third-party modules
 import requests
 
-__version__ = '1.1.29'
+__version__ = '1.1.30'
 logger = logging.getLogger(__name__)
 
 baseUrl = 'https://cloud.neocortix.com'
