cf99bcfbb2b03fbcb7d0cb9bdeb90a41f9f2a108
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-11-02 19:18:13 +0000

    installCustomSquid.sh has less-verbose installs

--- proxy/sproxy/squidWorker/installCustomSquid.sh
+++ proxy/sproxy/squidWorker/installCustomSquid.sh
@@ -2,27 +2,22 @@ set -ex
 version=${1:-squidVersionNotSet}
 
 apt-get -qq update > /dev/null
-#apt-get -qq install -y squid  > /dev/null
 
 scriptDirPath=`dirname "$0"`
 
 # install squid packages
 SQUID_PKG=4.13-10
-apt-get install -y libecap3 libltdl7 libnetfilter-conntrack3 libxml2 logrotate libdbi-perl
-apt-get install -y squid-langpack
+apt-get -qq install -y libecap3 libltdl7 libnetfilter-conntrack3 libxml2 logrotate libdbi-perl > /dev/null
+apt-get -qq install -y squid-langpack  > /dev/null
 pushd $scriptDirPath/pkg
-    dpkg --install squid-common_${SQUID_PKG}_all.deb
-    dpkg --install squid-openssl_${SQUID_PKG}_arm64.deb
-    dpkg --install squidclient_${SQUID_PKG}_arm64.deb
+    dpkg --install squid-common_${SQUID_PKG}_all.deb > /dev/null
+    dpkg --install squid-openssl_${SQUID_PKG}_arm64.deb  > /dev/null
+    dpkg --install squidclient_${SQUID_PKG}_arm64.deb  > /dev/null
 popd
-
-#tar -zxf $scriptDirPath/bin/squid.tar.gz -C /usr/sbin
-#cp -p $scriptDirPath/bin/squid /usr/sbin
 
 # configure squid for sslbump (https re-encryption)
 pushd .
     $scriptDirPath/configureSslBump.sh
 popd
-
 
 squid --version
