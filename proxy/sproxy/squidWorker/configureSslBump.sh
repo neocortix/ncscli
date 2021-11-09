ae571bc0c4b59b85d397ed91464f8d9a2d79abcb
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-11-09 19:51:13 +0000

    squidWorker configureSslBump.sh uses existing bump.key and bump.crt if found

--- proxy/sproxy/squidWorker/configureSslBump.sh
+++ proxy/sproxy/squidWorker/configureSslBump.sh
@@ -1,10 +1,16 @@
 set -ex
 cd /etc/squid/
 
-sudo openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout bump.key -out bump.crt \
-    -subj "/C=ZZ/ST=Area/L=Locality/O=Organization/OU=Unit/CN=example.com"
+scriptDirPath=`dirname "$0"`  # unfortunately not a full path
 
-sudo openssl x509 -in bump.crt -outform DER -out bump.der
+if test -f ~/$scriptDirPath/conf/bump.key; then
+    echo found existing bump.key
+else
+    echo no existing bump.key
+    sudo openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout bump.key -out bump.crt \
+        -subj "/C=ZZ/ST=Area/L=Locality/O=Organization/OU=Unit/CN=example.com"
+    sudo openssl x509 -in bump.crt -outform DER -out bump.der
+fi
 
 sudo openssl dhparam -outform PEM -out /etc/squid/bump_dhparam.pem 2048
 
@@ -15,5 +21,4 @@ sudo rm -rf /var/lib/squid/ssl_db  # in case it already existed
 sudo /usr/lib/squid/security_file_certgen -c -s /var/lib/squid/ssl_db -M 20MB
 
 # edit squid.conf to put some ssl-related directives at the end
-scriptDirPath=`dirname "$0"`  # unfortunately not a full path
 cat ~/$scriptDirPath/conf/sslBump.conf >> ~/$scriptDirPath/conf/squid.conf  # may require su privileges
