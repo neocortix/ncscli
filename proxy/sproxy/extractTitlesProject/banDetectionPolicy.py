5e73aa62399273865c29934e2da659a333d4575e
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-11-18 19:19:19 +0000

    extractTitlesProject/banDetectionPolicy.py ignores '/robots.txt?', prints message for empty body

--- proxy/sproxy/extractTitlesProject/banDetectionPolicy.py
+++ proxy/sproxy/extractTitlesProject/banDetectionPolicy.py
@@ -8,9 +8,13 @@ class banDetectionPolicy(object):
 
     def response_is_ban(self, request, response):
         if response.status not in self.NOT_BAN_STATUSES:
+            #print( '<request>', request, file=sys.stderr )
+            if '/robots.txt?' in request:
+                return False
             print( 'response_is_ban', response, file=sys.stderr )
             return True
         if response.status == 200 and not len(response.body):
+            print( 'response_is_ban got empty body', request, file=sys.stderr )
             return True
         return False
 
