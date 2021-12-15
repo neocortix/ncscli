5ff07f0e069b9a2860d14d4da727a818dece184b
Michael Coffey <22627691+xoffey@users.noreply.github.com>
2021-12-15 17:39:02 +0000

    jmeter/mergeBatchOutput.py has --multibatch functionality

--- jmeter/mergeBatchOutput.py
+++ jmeter/mergeBatchOutput.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-merge csv files produced by batchRunner examples, inserting worker-relative time stamps
+merge csv files produced by batchRunner examples, possibly adding columns
 """
 # standard library modules
 import argparse
 import csv
+import glob
 import json
 import logging
 import math
@@ -61,7 +62,7 @@ def findTimeStampBounds():
     # uses global iidByFrame, outputDir, and others
     outBounds = []
     for frameNum in iidByFrame:
-        inFilePath = outputDir + "/" + (resultsCsvPat % frameNum )
+        inFilePath = batchDirPath + "/" + (resultsCsvPat % frameNum )
         logger.debug( 'reading %s', inFilePath )
         try:
             rows = ingestCsv( inFilePath )
@@ -94,14 +95,14 @@ if __name__ == "__main__":
     ap.add_argument( '--mergedCsv', default='workers_merged.csv', help='file name for merged results csv file' )
     ap.add_argument( '--tsField', default='timeStamp', help='the name of the time stamp field in incoming csv files' )
     ap.add_argument( '--timeDiv', type=float, default=1000, help='timeStamp divisor (1000 for incoming ms; 1 for incoming seconds)' )
+    ap.add_argument( '--multibatch', type=boolArg, help='pass True for multiple batches, false for a single batch' )
     ap.add_argument( '--augment', type=boolArg, help='pass True if you want additional columns' )
     args = ap.parse_args()
 
     logger.info( 'merging data in directory %s', os.path.realpath(args.dataDirPath)  )
 
     outputDir = args.dataDirPath
-    launchedJsonFilePath = outputDir + "/recruitLaunched.json"
-    jlogFilePath = outputDir + "/batchRunner_results.jlog"
+    #launchedJsonFilePath = outputDir + "/recruitLaunched.json"
 
     mergedCsvFileName = args.mergedCsv
     resultsCsvPat = args.csvPat
@@ -109,6 +110,7 @@ if __name__ == "__main__":
     if tsDivisor <= 0:
         sys.exit( 'error: please pass a --timeDiv greater than 0')
 
+    '''
     launchedInstances = []
     with open( launchedJsonFilePath, 'r') as jsonInFile:
         try:
@@ -116,87 +118,100 @@ if __name__ == "__main__":
         except Exception as exc:
             logger.warning( 'could not load json (%s) %s', type(exc), exc )
     instancesByIid = { inst['instanceId']: inst for inst in launchedInstances }
+    '''
+    if args.multibatch:
+        batchDirPaths = glob.glob( os.path.join( outputDir, 'batch_*_*' ) )
+    else:
+        batchDirPaths = [outputDir]
+    logger.info( 'batchDirs: %s', batchDirPaths )
 
-    completedFrames = extractFrameInfo(jlogFilePath)
-    logger.debug( 'found %d frames', len(completedFrames) )
-    iidByFrame = { frame['frameNum']: frame['instanceId'] for frame in completedFrames }
-    logger.debug( 'iidByFrame: %s', iidByFrame )
-    frameNums = [int(frame['frameNum']) for frame in completedFrames]
-    maxFrameNum = max( frameNums )
-    #print( 'maxFrameNum', maxFrameNum )
-
-    timeStampBounds = findTimeStampBounds()
-    logger.debug( 'timeStampBounds %s', timeStampBounds )
-    if not timeStampBounds:
-        logger.warning( 'no timestamps found in any files')
-        sys.exit( 1 )
-    minTimeStamps = [bounds['min'] for bounds in timeStampBounds]
-    maxTimeStamps = [bounds['max'] for bounds in timeStampBounds]
-
-    minMinTimeStamp = int( min( minTimeStamps ) ) if minTimeStamps else 0
-    maxMaxTimeStamp = max( maxTimeStamps ) if maxTimeStamps else minMinTimeStamp + 1
-    logger.debug( 'minMinTimeStamp %d', minMinTimeStamp )
-
-    effDurs = [(bounds['max']-minMinTimeStamp)/args.timeDiv for bounds in timeStampBounds]
-    logger.debug( 'effDurs %s', effDurs )
-
-    maxSeconds = int( math.ceil( max(effDurs) ) )
-
-    extraFields = ['relTime', 'instanceId'] if args.augment else []
-    outFilePath = outputDir + '/' + mergedCsvFileName
     totRowsRead = 0
-    allThreadsCounter = np.zeros( [maxSeconds+1, maxFrameNum], dtype=np.int64 )
-    grpThreadsCounter = np.zeros( [maxSeconds+1, maxFrameNum], dtype=np.int64 )
-    outRows = []
+    fieldNames = None
+    writer = None
+    outFilePath = outputDir + '/' + mergedCsvFileName
     with open( outFilePath, 'w', newline='') as outfile:
-        fieldNames = None
-        writer = None
-        for frameNum in iidByFrame:
-            inFilePath = outputDir + "/" + (resultsCsvPat % frameNum )
-            iid = iidByFrame[ frameNum ]
-            logger.debug( 'reading %s', inFilePath )
-            try:
-                rows = ingestCsv( inFilePath )
-            except Exception as exc:
-                logger.warning( 'could not ingestCsv (%s) %s', type(exc), exc )
+        for batchDirPath in batchDirPaths:
+            jlogFilePath = batchDirPath + "/batchRunner_results.jlog"
+            if not os.path.isfile( jlogFilePath ):
+                logger.warning( 'did not find %s in %s', 'batchRunner_results.jlog', batchDirPath )
                 continue
-            if not rows:
-                logger.info( 'no rows in %s', inFilePath )
-                continue
-            logger.debug( 'read %d rows from %s', len(rows), inFilePath )
-            totRowsRead += len(rows)
-            timeStamps = [float(row[args.tsField]) for row in rows]
-            minTimeStamp = min(timeStamps)
-            if minTimeStamp > minMinTimeStamp + 60000:
-                logger.debug( 'frame %d started late', frameNum )
-            if not fieldNames:
-                fieldNames = list( rows[0].keys() ) + extraFields
-                logger.debug( 'columns:  %s', fieldNames )
-            if not writer:
-                if not fieldNames:
-                    logger.warning( 'no fieldNames')
+            completedFrames = extractFrameInfo(jlogFilePath)
+            logger.debug( 'found %d frames', len(completedFrames) )
+            if not completedFrames:
+                continue  # move on to next batch
+            iidByFrame = { frame['frameNum']: frame['instanceId'] for frame in completedFrames }
+            logger.debug( 'iidByFrame: %s', iidByFrame )
+            frameNums = [int(frame['frameNum']) for frame in completedFrames]
+            maxFrameNum = max( frameNums )
+            #print( 'maxFrameNum', maxFrameNum )
+
+            timeStampBounds = findTimeStampBounds()
+            logger.debug( 'timeStampBounds %s', timeStampBounds )
+            if not timeStampBounds:
+                logger.warning( 'no timestamps found in any files')
+                sys.exit( 1 )
+            minTimeStamps = [bounds['min'] for bounds in timeStampBounds]
+            maxTimeStamps = [bounds['max'] for bounds in timeStampBounds]
+
+            minMinTimeStamp = int( min( minTimeStamps ) ) if minTimeStamps else 0
+            maxMaxTimeStamp = max( maxTimeStamps ) if maxTimeStamps else minMinTimeStamp + 1
+            logger.debug( 'minMinTimeStamp %d', minMinTimeStamp )
+
+            effDurs = [(bounds['max']-minMinTimeStamp)/args.timeDiv for bounds in timeStampBounds]
+            logger.debug( 'effDurs %s', effDurs )
+
+            maxSeconds = int( math.ceil( max(effDurs) ) )
+
+            extraFields = ['relTime', 'instanceId'] if args.augment else []
+            allThreadsCounter = np.zeros( [maxSeconds+1, maxFrameNum], dtype=np.int64 )
+            grpThreadsCounter = np.zeros( [maxSeconds+1, maxFrameNum], dtype=np.int64 )
+            outRows = []
+            for frameNum in iidByFrame:
+                inFilePath = batchDirPath + "/" + (resultsCsvPat % frameNum )
+                iid = iidByFrame[ frameNum ]
+                logger.debug( 'reading %s', inFilePath )
+                try:
+                    rows = ingestCsv( inFilePath )
+                except Exception as exc:
+                    logger.warning( 'could not ingestCsv (%s) %s', type(exc), exc )
                     continue
-                writer = csv.DictWriter(outfile, fieldnames=fieldNames)
-                writer.writeheader()
-            for row in rows:
+                if not rows:
+                    logger.info( 'no rows in %s', inFilePath )
+                    continue
+                logger.debug( 'read %d rows from %s', len(rows), inFilePath )
+                totRowsRead += len(rows)
+                timeStamps = [float(row[args.tsField]) for row in rows]
+                minTimeStamp = min(timeStamps)
+                if minTimeStamp > minMinTimeStamp + 60000:
+                    logger.debug( 'frame %d started late', frameNum )
+                if not fieldNames:
+                    fieldNames = list( rows[0].keys() ) + extraFields
+                    logger.debug( 'columns:  %s', fieldNames )
+                if not writer:
+                    if not fieldNames:
+                        logger.warning( 'no fieldNames')
+                        continue
+                    writer = csv.DictWriter(outfile, fieldnames=fieldNames)
+                    writer.writeheader()
+                for row in rows:
+                    outRow = row
+                    relTime = (float(row[args.tsField])-minMinTimeStamp) / tsDivisor
+                    roundedTs = min( maxSeconds, round( relTime / 10 ) * 10 )
+                    allThreadsCounter[ roundedTs, frameNum-1 ] = int( row['allThreads'] )
+                    grpThreadsCounter[ roundedTs, frameNum-1 ] = int( row['grpThreads'] )
+                    if args.augment:
+                        outRow['relTime'] = round( relTime, 4 )
+                        outRow['instanceId'] = iid
+                    #outRow['threadName'] = outRow['threadName'] + '-' + str( frameNum )
+                    outRows.append( outRow )
+                    #writer.writerow( outRow )
+            for row in outRows:
                 outRow = row
                 relTime = (float(row[args.tsField])-minMinTimeStamp) / tsDivisor
                 roundedTs = min( maxSeconds, round( relTime / 10 ) * 10 )
-                allThreadsCounter[ roundedTs, frameNum-1 ] = int( row['allThreads'] )
-                grpThreadsCounter[ roundedTs, frameNum-1 ] = int( row['grpThreads'] )
-                if args.augment:
-                    outRow['relTime'] = round( relTime, 4 )
-                    outRow['instanceId'] = iid
-                #outRow['threadName'] = outRow['threadName'] + '-' + str( frameNum )
-                outRows.append( outRow )
-                #writer.writerow( outRow )
-        for row in outRows:
-            outRow = row
-            relTime = (float(row[args.tsField])-minMinTimeStamp) / tsDivisor
-            roundedTs = min( maxSeconds, round( relTime / 10 ) * 10 )
-            nThreads = allThreadsCounter[ roundedTs, :].sum()
-            outRow['allThreads'] = nThreads
-            nThreads = grpThreadsCounter[ roundedTs, :].sum()
-            outRow['grpThreads'] = nThreads
-            writer.writerow( outRow )
-    logger.debug( 'totRowsRead: %d', totRowsRead )
+                nThreads = allThreadsCounter[ roundedTs, :].sum()
+                outRow['allThreads'] = nThreads
+                nThreads = grpThreadsCounter[ roundedTs, :].sum()
+                outRow['grpThreads'] = nThreads
+                writer.writerow( outRow )
+        logger.debug( 'totRowsRead: %d', totRowsRead )
